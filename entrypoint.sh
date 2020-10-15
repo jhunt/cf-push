72ed0d6979d88d1f81894f77218110a2ad16edd0
James Hunt <james@niftylogic.com>
2020-10-15 16:28:02 -0400

    Support Insecure CF API Endpoints

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -eu
-cf api ${INPUT_API}
+cf_opts=
+if [ "x${INPUT_VALIDATE}" = "xfalse" ]; then
+  cf_opts="--skip-ssl-validation"
+fi
+cf api ${INPUT_API} ${cf_opts}
 CF_USERNAME=${INPUT_USERNAME} CF_PASSWORD=${INPUT_PASSWORD} cf auth
 cf target -o ${INPUT_ORG} -s ${INPUT_SPACE}
 cf push -f ${INPUT_MANIFEST}
