9cdfa3ca3ce8e57ea73ef692af02e5e380128ffd
rangulvers <5235430+rangulvers@users.noreply.github.com>
2021-12-02 11:38:22 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/sh -l
 set -eu
-# cf_opts= 
-# if [ "x${INPUT_VALIDATE}" = "xfalse" ]; then
-#   cf_opts="--skip-ssl-validation"
-# fi
+cf_opts= 
+if [ "x${INPUT_VALIDATE}" = "xfalse" ]; then
+  cf_opts="--skip-ssl-validation"
+fi
 #/home/runner/work/cf_python_test_app/cf_python_test_app
 echo ${INPUT_WORKDIR}
 
@@ -15,7 +15,7 @@ fi
 #change dir to push only this app
 #cd {INPUT_WORKINGDIR}
 
-cf api ${INPUT_API} ${cf_opts}
-CF_USERNAME=${INPUT_USERNAME} CF_PASSWORD=${INPUT_PASSWORD} cf auth
-cf target -o ${INPUT_ORG} -s ${INPUT_SPACE}
-cf push -f ${INPUT_MANIFEST}
+# cf api ${INPUT_API} ${cf_opts}
+# CF_USERNAME=${INPUT_USERNAME} CF_PASSWORD=${INPUT_PASSWORD} cf auth
+# cf target -o ${INPUT_ORG} -s ${INPUT_SPACE}
+# cf push -f ${INPUT_MANIFEST}
