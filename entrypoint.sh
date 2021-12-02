1a636ded557b6ae3e0f4e958a7e2add2d0fb4741
rangulvers <5235430+rangulvers@users.noreply.github.com>
2021-12-02 11:54:13 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,15 @@ if [ "x${INPUT_VALIDATE}" = "xfalse" ]; then
   cf_opts="--skip-ssl-validation"
 fi
 #/home/runner/work/cf_python_test_app/cf_python_test_app
-echo ${INPUT_APPKDIR}
+# echo ${INPUT_APPKDIR}
 
-if [ -z ${INPUT_APPKDIR+x} ]; then 
+if [ -z ${INPUT_APPDIR+x} ]; then 
   echo "WORKDIR is not set. Staying in Root Dir"; else 
-    echo ${INPUT_APPKDIR}
-    cd ${INPUT_APPKDIR}
+    echo ${INPUT_APPDIR}
+    cd ${INPUT_APPDIR}
 fi
 # change dir to push only this app
-cd {INPUT_WORKINGDIR}
+# cd {INPUT_APPDIR}
 
 cf api ${INPUT_API} ${cf_opts}
 CF_USERNAME=${INPUT_USERNAME} CF_PASSWORD=${INPUT_PASSWORD} cf auth
