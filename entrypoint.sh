04d1d268d30c352f9bfa904604fcc44b57e9dc97
rangulvers <5235430+rangulvers@users.noreply.github.com>
2021-12-02 11:48:14 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-# set -eu
+set -eu
 cf_opts= 
 if [ "x${INPUT_VALIDATE}" = "xfalse" ]; then
   cf_opts="--skip-ssl-validation"
@@ -7,15 +7,15 @@ fi
 #/home/runner/work/cf_python_test_app/cf_python_test_app
 echo ${INPUT_APPKDIR}
 
-# if [ -z ${INPUT_APPKDIR+x} ]; then 
-#   echo "WORKDIR is not set. Staying in Root Dir"; else 
-#     echo ${INPUT_APPKDIR}
-#     cd ${INPUT_APPKDIR}
-# fi
-#change dir to push only this app
-#cd {INPUT_WORKINGDIR}
+if [ -z ${INPUT_APPKDIR+x} ]; then 
+  echo "WORKDIR is not set. Staying in Root Dir"; else 
+    echo ${INPUT_APPKDIR}
+    cd ${INPUT_APPKDIR}
+fi
+# change dir to push only this app
+cd {INPUT_WORKINGDIR}
 
-# cf api ${INPUT_API} ${cf_opts}
-# CF_USERNAME=${INPUT_USERNAME} CF_PASSWORD=${INPUT_PASSWORD} cf auth
-# cf target -o ${INPUT_ORG} -s ${INPUT_SPACE}
-# cf push -f ${INPUT_MANIFEST}
+cf api ${INPUT_API} ${cf_opts}
+CF_USERNAME=${INPUT_USERNAME} CF_PASSWORD=${INPUT_PASSWORD} cf auth
+cf target -o ${INPUT_ORG} -s ${INPUT_SPACE}
+cf push -f ${INPUT_MANIFEST}
